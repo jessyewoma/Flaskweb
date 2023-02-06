678113118fed97050e4152ff81cbd857826d5748
jessyewoma <61324491+jessyewoma@users.noreply.github.com>
2023-02-05 22:21:49 -0600

    Create flasktest.py

---
+++ flasktest.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "I am almost a Deveops Engineer!!"
+
+
+
+app.run(host="0.0.0.0", port=5000)
