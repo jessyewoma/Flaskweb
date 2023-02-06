52e3bc401381d8ed003c871217079500dcbaa277
jessyewoma <61324491+jessyewoma@users.noreply.github.com>
2023-02-05 22:28:08 -0600

    Update flasktest.py

--- flasktest.py
+++ flasktest.py
@@ -1,11 +1,8 @@
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route("/")
-def index():
-    return "I am almost a Deveops Engineer!!"
+def hello():
+    return "I am almost a Devops Engineer!"
 
-
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
